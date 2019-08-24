7e62210f2f9b8bd30b0ca9d78f9c04ed58b8ecfc
liftingturn <0oooceanhigh@gmail.com>
2019-08-24 14:13:43 +0900

    블록 떨어지면 삭제까지 완료(linear옵션 적용 안됨)

--- script_hj.js
+++ script_hj.js
@@ -1,25 +1,26 @@
 var main = document.getElementById('main');
 var wordList = ['가나', '다라', '마바', '사아', '자차', '카타', '파하'];
 
-function Block(word) {
+function Block(word,fallSec) {
     this.element = document.createElement('div');
-    this.element.className = 'blocks';
-    this.element.innerText = word;
-    this.element.id = word;
-    this.element.style.left = Math.ceil(Math.random() * 85) + '%';
-    this.element.style.display = 'block';
-   
-    this.element.animate([
-        // keyframes
-        { transform: 'translateY(0px)' },
-        { transform: 'translateY(700px)' }
-    ], 3000,(function() {console.log('end');
-      $('.blocks').remove(); 
-
-       })()   
-        );
-        main.appendChild(this.element);
+    let ele = this.element;
+    ele.className = 'blocks';
+    ele.innerText = word;
+    ele.id = word;
+    ele.style.left = 210+Math.floor((Math.random()*1300))+'px';
+    ele.style.display = 'inline-block';
+    ele.style.position = 'absolute';
+   // console.log(this);
+ 
+    ele.style.WebkitAnimation = "mymove "+fallSec+'s'; // Code for Chrome, Safari and Opera
+    ele.animation = "mymove "+fallSec+'s';     // Standard syntax
+    ele.addEventListener("webkitAnimationEnd", myEndFunction);
+    //ele.style.animation-timing-function = 'linear';
+    main.appendChild(this.element);
+     
 }
+
+
 
 // 프로토타입 객체에 메소드 정의
 Block.prototype.setWord = function (word) {
@@ -33,13 +34,23 @@ Block.prototype.getName = function () {
 Block.prototype.remove =function(){
     this.element.remove();
 }
+Block.prototype.setZindex =function(num){
+    this.element.style.zIndex =num;
+}
+function myEndFunction() {
+    console.log('end');
+    this.remove();
+  }
+
+
 
 window.addEventListener('load', function () {
+    var cnt = 1;
     setInterval(function () {
         var word = wordList.shift();
-        var block1 = new Block(word);
-
-        console.log($('.blocks'));
-    }, 2000);
+        var fallSec = 3;
+        eval('var block'+cnt +'= new Block(word,fallSec)');      
+        cnt++;
+    }, 1000);
 })
 
